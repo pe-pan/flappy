4a15aa2a8d0f47a8fb5430667723cd1beedb5f82
pe_pan <pe.pan@centrum.cz>
2016-02-11 11:21:38 +0100

    REF: code reuse - introducing hideMenuAndFinishIntro method; when finishing intro, music needs to go off

--- mobile/src/main/java/com/mz800/flappy/IntroActivity.java
+++ mobile/src/main/java/com/mz800/flappy/IntroActivity.java
@@ -93,6 +93,7 @@ public class IntroActivity extends FlappyActivity {
     }
 
     public void exitGame(View view) {
+        hideMenuAndFinishIntro();
         Device.music.destroy();
         super.onBackPressed();
     }
